e3320bd5a4c4ddc50f151cd55a1c9a3a544a1633
J1Walters <150163391+J1Walters@users.noreply.github.com>
2024-07-30 16:07:01 +0100

    renamed

---
+++ Home.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title='Dashboard - Home'
+    page_icon='📊'
+)
+
+st.sidebar.success('Please select a page from above.')
+
+st.title('Analysing the UK Tech Job Market Using Natural Language Processing')
+st.write(
+    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+)
