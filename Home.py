918a0b3e0138787d68463dae8ddb88e50c896635
J1Walters <150163391+J1Walters@users.noreply.github.com>
2024-07-31 17:03:06 +0100

    welcome message

--- Home.py
+++ Home.py
@@ -8,6 +8,6 @@ st.set_page_config(
 st.sidebar.success('Please select a page from above.')
 
 st.title('Analysing the UK Tech Job Market Using Natural Language Processing')
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+st.markdown(
+    'Welcome to the job insights dashboard. Please select a page from the sidebar.'
 )
