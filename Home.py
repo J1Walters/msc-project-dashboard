d6bd8e7a651d224791cf147a7a5bf6b62635ea8c
J1Walters <150163391+J1Walters@users.noreply.github.com>
2024-08-30 18:01:32 +0100

    updated sidebar

--- Home.py
+++ Home.py
@@ -5,7 +5,12 @@ import pandas as pd
 if 'loc_data' not in st.session_state:
     st.session_state.loc_data = pd.read_csv('./data/location.csv')
 if 'ent_data' not in st.session_state:
-    st.session_state.ent_data = pd.read_csv('./data/entities.csv')
+    st.session_state.ent_data = pd.read_csv('./data/multihash_maxout_vec_ents_clean.csv')
+
+# Load data
+loc_df = st.session_state.loc_data
+# Get total number of job listings in dataset
+num_jobs = loc_df.shape[0]
 
 st.set_page_config(
     page_title='Dashboard - Home',
@@ -13,6 +18,8 @@ st.set_page_config(
 )
 
 st.sidebar.success('Please select a page from above.')
+st.sidebar.write('Data sourced from Gradcracker and Indeed.')
+st.sidebar.write(f'Total number of jobs: {num_jobs}')
 
 st.title('Analysing the UK Tech Job Market Using Natural Language Processing')
 st.markdown(
