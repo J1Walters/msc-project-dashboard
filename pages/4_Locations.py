4486370ef4bb6b4fc2077d13420decbf3dc5147b
J1Walters <150163391+J1Walters@users.noreply.github.com>
2024-08-31 15:39:41 +0100

    added info on remote and multi location jobs

--- pages/4_Locations.py
+++ pages/4_Locations.py
@@ -5,16 +5,34 @@ import plotly.express as px
 # Load in data
 loc_df = st.session_state.loc_data
 
+# Get dataframe with settlement counts and proportions
+loc_counts = loc_df['settlement'].value_counts()
+loc_props = loc_df['settlement'].value_counts(normalize=True)
+loc_count_props = pd.concat([loc_counts, loc_props], axis=1)
+
+# Get number of jobs in multiple locations and remote jobs
+num_multi = loc_df.loc[loc_df['location'] == 'Multiple Locations'].shape[0]
+num_remote = loc_df.loc[loc_df['location'] == 'Remote'].shape[0]
+
+# Group by settlement and average out any descrepencies in latitude and longitude
 geo_df = loc_df.groupby('settlement').agg({'lat':'mean', 'long':'mean', 'settlement':'count'})
 geo_df.rename(columns={'settlement':'num_jobs'}, inplace=True)
 
+# Get counts for each nation within UK
 states_df = loc_df['state'].value_counts()
 
-map_fig = px.scatter_mapbox(geo_df, lat='lat', lon='long', size='num_jobs', size_max=30,
-                        hover_name=geo_df.index, zoom=4.8, mapbox_style='open-street-map'
+# Create map figure
+map_fig = px.density_mapbox(geo_df, lat='lat', lon='long', z='num_jobs', opacity=0.9, radius=30,
+                        hover_name=geo_df.index, zoom=4.8, mapbox_style='open-street-map',
+                        labels={
+                            'lat':'Latitude',
+                            'long':'Longitude',
+                            'num_jobs':'Number of Jobs'
+                        }
 )
 map_fig.update_layout(height=750)
 
+# Create pie chart
 pie_fig = px.pie(states_df, values='count', names=states_df.index)
 
 st.set_page_config(
@@ -22,6 +40,7 @@ st.set_page_config(
     page_icon='📍'
 )
 
+# Sidebar
 st.sidebar.write('Data sourced from Gradcracker and Indeed.')
 
 st.markdown(
@@ -32,14 +51,20 @@ st.markdown(
     '### Map'
 )
 
+# Plot map
 st.plotly_chart(
     map_fig, use_container_width=True
 )
+
+# Display number of jobs in multiple locations and remote
+st.write(f'Number of Jobs in Multiple Locations: {num_multi}')
+st.write(f'Number of Remote Jobs: {num_remote}')
 
 st.markdown(
     '### National Breakdown'
 )
 
+# Plot pie chart
 st.plotly_chart(
     pie_fig, use_container_width=True
 )
@@ -49,9 +74,10 @@ st.markdown(
 )
 
 st.dataframe(
-    loc_df['settlement'].value_counts(), use_container_width=True,
+    loc_count_props, use_container_width=True,
     column_config={
         'settlement':'Location',
-        'count':'Count'
+        'count':'Count',
+        'proportion':'Proportion'
     }
 )
