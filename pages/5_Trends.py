da42ca10b665f8a9e2861c0016c840e149afa931
J1Walters <150163391+J1Walters@users.noreply.github.com>
2024-09-02 20:20:43 +0100

    added increase and decrease functions

--- pages/5_Trends.py
+++ pages/5_Trends.py
@@ -7,6 +7,28 @@ year_df = pd.read_csv('./data/yearly_trend.csv')
 month_df = pd.read_csv('./data/monthly_trend.csv')
 week_df = pd.read_csv('./data/weekly_trend.csv')
 
+# Define functions
+def get_top_increase(df, skill, num=5, min_count=5):
+    """Get the specified number of the top increasing skills"""
+    filtered_df = df.loc[df['type'] == skill]
+    top_increase = (filtered_df.loc[(filtered_df['perc_change'] > 0) & (filtered_df['count'] > min_count)]
+                    .sort_values(by='perc_change', ascending=False)
+                    .head(num)
+    )
+    return top_increase
+
+def get_top_decrease(df, skill, num=5, min_count=5):
+    """Get the specified number of the top decreasing skills"""
+    filtered_df = df.loc[df['type'] == skill]
+    top_decrease = (filtered_df.loc[(filtered_df['perc_change'] < 0) & (filtered_df['count'] > min_count)]
+                    .sort_values(by='perc_change', ascending=True)
+                    .head(num)
+    )
+    return top_decrease
+
+def create_plot(df):
+    pass
+
 st.set_page_config(
     page_title='Dashboard - Trends',
     page_icon='📈',
